5509332617e8f1b6e33867b727a54311452ebcb3
Timofey Peshin <timoffex@gmail.com>
2023-03-12 22:35:00 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import '$lib/global.scss';
+  import '$lib/layout/layout-styles.scss';
+</script>
+
+<div class="main-content">
+  <div class="grid-container">
+    <div class="grid-area-yt-gaming tile"></div>
+    <div class="grid-area-salem tile"></div>
+    <div class="grid-area-sj-sharks tile"></div>
+    <div class="grid-area-ninja tile"></div>
+    <div class="grid-area-aapi tile"></div>
+    <div class="grid-area-nymphs-tower tile"></div>
+    <div class="grid-area-eyes tile"></div>
+    <div class="grid-area-reel tile"></div>
+    <div class="grid-area-pokemon tile"></div>
+    <div class="grid-area-amiibo tile"></div>
+    <div class="grid-area-play-magazine tile"></div>
+    <div class="grid-area-wattam tile"></div>
+  </div>
+</div>
