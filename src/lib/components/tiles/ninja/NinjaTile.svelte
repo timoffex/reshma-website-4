ce182e9fdef90c2ba60fd6e11b56b198a639c151
Timofey Peshin <timoffex@gmail.com>
2023-03-14 21:30:32 -0700

    Ninja tile

---
+++ src/lib/components/tiles/ninja/NinjaTile.svelte
@@ -0,0 +1,36 @@
+<script>
+	import TilePicture from "$lib/layout/TilePicture.svelte";
+  import ButtonTile from "$lib/widgets/ButtonTile.svelte";
+
+  import NinjaLogoWebp712 from './ninja-logo.png?w=712&webp&imagetools';
+  import NinjaLogoWebp356 from './ninja-logo.png?w=356&webp&imagetools';
+  import NinjaLogoWebp175 from './ninja-logo.png?w=175&webp&imagetools';
+  import NinjaLogoJpeg356 from './ninja-logo.png?w=356&jpeg&imagetools';
+  import NinjaLogoJpeg175 from './ninja-logo.png?w=175&jpeg&imagetools';
+
+</script>
+
+<ButtonTile areaName="ninja">
+  <TilePicture
+      slot="content"
+      imgClass="fill-contain black-background"
+      picture={{
+        case: 'gridA-3',
+        alt: 'Ninja (streamer)',
+
+        webp712: NinjaLogoWebp712,
+        webp356: NinjaLogoWebp356,
+        webp175: NinjaLogoWebp175,
+
+        jpeg356: NinjaLogoJpeg356,
+        jpeg175: NinjaLogoJpeg175,
+      }} />
+  
+  <div slot="description">
+    <p>
+      Designed numerous YouTube thumbnails for eSports personality,
+      Ninja.
+    </p>
+    <p>Agency: XX Artists</p>
+  </div>
+</ButtonTile>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 
   import Navbar from '$lib/components/navigation/Navbar.svelte';
 	import AnimatedTigerTile from '$lib/components/tiles/aapi-heritage/AnimatedTigerTile.svelte';
+	import NinjaTile from '$lib/components/tiles/ninja/NinjaTile.svelte';
 	import SjSharksTile from '$lib/components/tiles/sj-sharks/SjSharksTile.svelte';
 	import FlyoutScrim from '$lib/flyout/FlyoutScrim.svelte';
 	import EmailAddress from '$lib/widgets/EmailAddress.svelte';
@@ -16,7 +17,7 @@
     <div class="grid-area-yt-gaming tile"></div>
     <div class="grid-area-salem tile"></div>
     <SjSharksTile />
-    <div class="grid-area-ninja tile"></div>
+    <NinjaTile />
     <AnimatedTigerTile />
     <div class="grid-area-nymphs-tower tile"></div>
     <div class="grid-area-eyes tile"></div>
