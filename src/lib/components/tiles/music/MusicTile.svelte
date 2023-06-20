3d683bc07c3d8c35f09b8c082e5d04ca52c1d0e4
Timofey Peshin <timoffex@gmail.com>
2023-06-19 23:27:37 -0700

    Different image for music client tile

--- src/lib/components/tiles/music/MusicTile.svelte
+++ src/lib/components/tiles/music/MusicTile.svelte
@@ -1,25 +1,17 @@
 <script>
-	import TilePicture from '$lib/layout/TilePicture.svelte';
+	import { gridASizesList } from '$lib/layout/layout';
+	import GeneratedImage from '$lib/widgets/GeneratedImage.svelte';
 
-	import RecordPlayerWebp540 from './record-player2-1080.webp?w=540&imagetools';
-	import RecordPlayerWebp270 from './record-player2-1080.webp?w=270&imagetools';
-	import RecordPlayerWebp135 from './record-player2-1080.webp?w=135&imagetools';
-	import RecordPlayerJpeg540 from './record-player2-1080.webp?w=540&jpeg&imagetools';
-	import RecordPlayerJpeg270 from './record-player2-1080.webp?w=270&jpeg&imagetools';
-	import RecordPlayerJpeg135 from './record-player2-1080.webp?w=135&jpeg&imagetools';
+	import { MusicClientsRecord } from '$lib/generated-images';
 </script>
 
 <a href="/music" class="display-block tile grid-area-music">
-	<TilePicture
+	<GeneratedImage
 		imgClass="fill-cover"
-		alt="Music Clients"
-		loading="lazy"
-		subgrid="A"
-		columns={2}
-		mobileWebp={RecordPlayerWebp270}
-		mobileJpeg={RecordPlayerJpeg270}
-		webpWidthSrcset={`${RecordPlayerWebp540} 540w, ${RecordPlayerWebp270} 270w, ${RecordPlayerWebp135} 135w`}
-		jpegWidthSrcset={`${RecordPlayerJpeg540} 540w, ${RecordPlayerJpeg270} 270w, ${RecordPlayerJpeg135} 135w`}
-		jpegSrc={RecordPlayerJpeg135}
+		image={{
+			alt: 'Music Clients',
+			...MusicClientsRecord
+		}}
+		sizes={gridASizesList(2)}
 	/>
 </a>
