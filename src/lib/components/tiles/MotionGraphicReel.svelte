cda70570081fc5696e03510b37408a50e8d494e5
Timofey Peshin <timoffex@gmail.com>
2023-06-21 16:31:23 -0700

    Update reel link

--- src/lib/components/tiles/MotionGraphicReel.svelte
+++ src/lib/components/tiles/MotionGraphicReel.svelte
@@ -8,7 +8,7 @@
 		class="fill-cover"
 		width="560"
 		height="315"
-		src="https://www.youtube.com/embed/KQ7J0P__0Qc"
+		src="https://www.youtube.com/embed/r1Wk07f2V_c"
 		title="YouTube video player"
 		frameborder="0"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
