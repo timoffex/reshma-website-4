941abc8aa3b8a6f3d4067922a7561694f1eb0d50
Timofey Peshin <timoffex@gmail.com>
2023-06-22 00:44:57 -0700

    WeTransfer button and description

--- src/lib/components/tiles/WeTransferTile.svelte
+++ src/lib/components/tiles/WeTransferTile.svelte
@@ -1,11 +1,16 @@
 <script>
-	import { fadeInOut } from '$lib/fade-in-out';
+	import ButtonTile from '$lib/widgets/ButtonTile.svelte';
 
 	if (typeof window !== 'undefined') import('@lottiefiles/lottie-player');
 
 	const LOTTIE_PATH = 'wetransfer2.jsonlottie';
 </script>
 
-<div class="tile grid-area-wetransfer bg-black highlight:bg-red" use:fadeInOut>
-	<lottie-player autoplay loop src={LOTTIE_PATH} class="fill image-color-on-hover" />
-</div>
+<ButtonTile areaName="wetransfer" buttonClass="bg-black highlight:bg-red">
+	<lottie-player slot="content" autoplay loop src={LOTTIE_PATH} class="fill image-color-on-hover" />
+
+	<p slot="description">
+		I created motion graphics for WeTransfer, Collect, and WEPRESENT's Twitter, Instagram and
+		LinkedIn.
+	</p>
+</ButtonTile>
