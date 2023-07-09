fd7e26b628c7059ad53a92ed59271c8ea0515d99
Timofey Peshin <timoffex@gmail.com>
2023-07-08 19:43:18 -0700

    WeTransfer tile click loads page

--- src/lib/components/tiles/WeTransferTile.svelte
+++ src/lib/components/tiles/WeTransferTile.svelte
@@ -1,16 +1,9 @@
 <script>
-	import ButtonTile from '$lib/widgets/ButtonTile.svelte';
-
 	if (typeof window !== 'undefined') import('@lottiefiles/lottie-player');
 
 	const LOTTIE_PATH = 'wetransfer3.jsonlottie';
 </script>
 
-<ButtonTile areaName="wetransfer" buttonClass="bg-black highlight:bg-red">
-	<lottie-player slot="content" autoplay loop src={LOTTIE_PATH} class="fill image-color-on-hover" />
-
-	<p slot="description">
-		I created motion graphics for WeTransfer, Collect, and WEPRESENT's Twitter, Instagram and
-		LinkedIn. See <a href="/wetransfer">here</a>.
-	</p>
-</ButtonTile>
+<a href="/wetransfer" class="display-block tile grid-area-wetransfer bg-black highlight:bg-red">
+	<lottie-player autoplay loop src={LOTTIE_PATH} class="fill image-color-on-hover" />
+</a>
