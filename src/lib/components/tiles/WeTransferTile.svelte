5f6c4a01f9b3eeb5fd92c8b9b0c619a7e8b39897
Timofey Peshin <timoffex@gmail.com>
2023-07-08 23:18:20 -0700

    WeTransferTile fade-in-out

--- src/lib/components/tiles/WeTransferTile.svelte
+++ src/lib/components/tiles/WeTransferTile.svelte
@@ -1,9 +1,15 @@
 <script>
+	import { fadeInOut } from '$lib/fade-in-out';
+
 	if (typeof window !== 'undefined') import('@lottiefiles/lottie-player');
 
 	const LOTTIE_PATH = 'wetransfer3.jsonlottie';
 </script>
 
-<a href="/wetransfer" class="display-block tile grid-area-wetransfer bg-black highlight:bg-red">
+<a
+	href="/wetransfer"
+	class="display-block tile grid-area-wetransfer bg-black highlight:bg-red"
+	use:fadeInOut
+>
 	<lottie-player autoplay loop src={LOTTIE_PATH} class="fill image-color-on-hover" />
 </a>
