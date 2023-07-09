c65b28b6d0d888fa85be079f0a0d1885e92558bb
Timofey Peshin <timoffex@gmail.com>
2023-07-08 19:50:30 -0700

    Remove smooth scroll on route change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
-<script>
+<script lang="ts">
 	import '$lib/global.scss';
 	import '$lib/layout/layout-styles.scss';
+
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	/* Use smooth-scroll except when navigating between pages. */
+
+	let root: HTMLElement;
+	onMount(() => {
+		root = document.getElementsByTagName('html')[0];
+	});
+
+	beforeNavigate(() => {
+		root.style.scrollBehavior = 'auto';
+	});
+
+	afterNavigate(() => {
+		root.style.scrollBehavior = 'smooth';
+	});
 </script>
 
 <slot />
