52c936c2536969c45721ca15ee1f7cdd33ae13d7
Timofey Peshin <timoffex@gmail.com>
2025-04-11 17:08:45 -0700

    no more links in NavBar; click name to go home

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -1,35 +1,21 @@
-<div class="container">
-  <div class="links">
-    <a href="/">01 Home</a>
-    <a href="/#contact">02 Contact</a>
-  </div>
-  <div class="name">Reshma</div>
-</div>
+<h1 class="name">
+  <a href="/">Reshma</a>
+</h1>
 
 <style lang="scss">
-  .container {
-    display: flex;
-  }
-
-  .links {
-    display: flex;
-    flex-grow: 1;
-    flex-wrap: wrap;
-
-    justify-content: space-around;
-    align-items: center;
-
-    a {
-      text-decoration: none;
-    }
-  }
-
   .name {
-    flex-basis: 0;
-    flex-grow: 1;
-
     font-family: 'Clearface';
     text-align: right;
-    font-size: 4em;
+    font-size: 4rem;
+
+    // Reset h1 default styles.
+    font-weight: normal;
+    margin: 0;
+
+    // Remove link styling.
+    & a {
+      text-decoration: none;
+      color: inherit;
+    }
   }
 </style>
