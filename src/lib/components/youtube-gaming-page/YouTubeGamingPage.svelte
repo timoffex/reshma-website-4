f870895faca4a1bd2ec1591ed1ab4eca8e4de160
Timofey Peshin <timoffex@gmail.com>
2025-04-11 19:32:18 -0700

    extra info at bottom of YTG page

--- src/lib/components/youtube-gaming-page/YouTubeGamingPage.svelte
+++ src/lib/components/youtube-gaming-page/YouTubeGamingPage.svelte
@@ -22,11 +22,12 @@
     <div class="yt-gaming-grid__1x1 yt-gaming-grid__text">
       <p>
         In 2020, I was brought on board XX Artists' Design team as a Motion
-        Designer. I was key to the YouTube Gaming social team because of my
-        previous work in the Gaming industry, and familiarity with social first
-        Gaming content. During my time on the team, @YouTubeGaming saw steady
-        growth on Twitter, growing from 1.5 million followers to over 2.3
-        million in the span of three years.
+        Designer for my experience in the gaming industry.
+      </p>
+      <p>
+        During my time on the team, @YouTubeGaming saw steady growth on Twitter,
+        growing from 1.5 million followers to over 2.3 million in the span of
+        three years.
       </p>
     </div>
 
@@ -41,10 +42,7 @@
       <p>
         As part of YouTube Gaming's Always On social team, I created crisp
         motion graphics, fun GIFs and illustrations for numerous campaigns
-        pitched in-house or for paid social. I helped maintain and devise a
-        cohesive brand identity across all assets. Some campaigns I worked on
-        include Minecraft Museum, #YouTubeGameOn, Pride, Earth Day, uTure, and
-        League of Legends Worlds 2022, among others.
+        pitched in-house and for paid social.
       </p>
     </div>
     <div class="yt-gaming-grid__1x1">
@@ -53,6 +51,26 @@
         <source src={YtgBento3Mp4} type="video/mp4" />
       </video>
     </div>
+  </div>
+
+  <div class="tile bottom-tile">
+    <p class="text-align-center">
+      I helped maintain and devise a cohesive brand identity across all assets.
+      Some campaigns I worked on include #MinecraftMuseum, #YouTubeGameOn,
+      Pride, Earth Day, uTure, and League of Legends Worlds 2022, among others.
+    </p>
+    <p class="text-align-center">
+      <a
+        href="https://www.figma.com/proto/IswuUG18DXTh663103z0oo/Reshma-Zachariah's-work-for-YouTube-Gaming?node-id=1-3&p=f&t=O19d8BdL2pUzvDD7-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
+        class="bottom-tile__link"
+        >See More on Figma.</a
+      >
+    </p>
+    <p class="text-align-right text-small">
+      Agency: XX Artists<br />
+      Creative Director: Mackenna Millet, Erin Taj<br />
+      YouTube Product Marketing Manager: Kate Bryant<br />
+    </p>
   </div>
 </div>
 
@@ -92,5 +110,28 @@
     grid-row: span 1;
     grid-column: span 2;
     aspect-ratio: 16 / 9;
+  }
+
+  .text-align-center {
+    text-align: center;
+  }
+
+  .text-align-right {
+    text-align: right;
+  }
+
+  .bottom-tile {
+    padding: 0 7%;
+
+    background-color: #212121;
+    color: white;
+  }
+
+  .bottom-tile__link:link {
+    color: cyan;
+  }
+
+  .bottom-tile__link:visited {
+    color: magenta;
   }
 </style>
