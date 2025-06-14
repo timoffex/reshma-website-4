fd2aaf406d3c556a6db247b5cf2a893c81b6725f
Timofey Peshin <timoffex@gmail.com>
2025-06-13 22:34:58 -0700

    make SJ sharks tile link out to IG

--- src/lib/components/tiles/sj-sharks/SjSharksTile.svelte
+++ src/lib/components/tiles/sj-sharks/SjSharksTile.svelte
@@ -1,6 +1,6 @@
 <script>
+  import { fadeInOut } from '$lib/fade-in-out';
   import TilePicture from '$lib/layout/TilePicture.svelte';
-  import ButtonTile from '$lib/widgets/ButtonTile.svelte';
 
   import ReshmaWebp720 from './reshma-zachariah-720.webp';
   import ReshmaWebp356 from './reshma-zachariah-356.webp';
@@ -10,28 +10,21 @@
   import ReshmaJpeg175 from './reshma-zachariah-175.webp?jpeg&imagetools';
 </script>
 
-<ButtonTile areaName="sj-sharks">
-  {#snippet content()}
-    <TilePicture
-      imgClass="fill-cover image-color-on-hover"
-      alt="Reshma painting at an SJ Sharks game"
-      subgrid="A"
-      columns={3}
-      mobileWebp={ReshmaWebp356}
-      mobileJpeg={ReshmaJpeg356}
-      webpWidthSrcset={`${ReshmaWebp720} 720w, ${ReshmaWebp356} 356w, ${ReshmaWebp175} 175w`}
-      jpegWidthSrcset={`${ReshmaJpeg356} 356w, ${ReshmaJpeg175} 175w`}
-      jpegSrc={ReshmaJpeg175}
-    />
-  {/snippet}
-  {#snippet description()}
-    <div>
-      <p>
-        Reshma Zachariah is a Motion Design Artist and Illustrator based in
-        California.
-      </p>
-      <p>Learn about her works by clicking anywhere on this page.</p>
-      <p>@reshmushroom on all socials.</p>
-    </div>
-  {/snippet}
-</ButtonTile>
+<a
+  target="_blank"
+  href="https://www.instagram.com/reshmushroom/"
+  class="display-block tile grid-area-sj-sharks"
+  use:fadeInOut
+>
+  <TilePicture
+    imgClass="fill-cover image-color-on-hover"
+    alt="Reshma painting at an SJ Sharks game"
+    subgrid="A"
+    columns={3}
+    mobileWebp={ReshmaWebp356}
+    mobileJpeg={ReshmaJpeg356}
+    webpWidthSrcset={`${ReshmaWebp720} 720w, ${ReshmaWebp356} 356w, ${ReshmaWebp175} 175w`}
+    jpegWidthSrcset={`${ReshmaJpeg356} 356w, ${ReshmaJpeg175} 175w`}
+    jpegSrc={ReshmaJpeg175}
+  />
+</a>
