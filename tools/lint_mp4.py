8047551d55fdaaf738a25cc20879efb07f95825e
Timofey Peshin <timoffex@gmail.com>
2025-06-15 00:02:16 -0700

    lint_mp4

---
+++ tools/lint_mp4.py
@@ -0,0 +1,57 @@
+import pathlib
+
+import fire
+from rich.console import Console
+from rich.markup import escape
+import subprocess
+from ntpath import extsep
+
+
+console = Console(stderr=True)
+
+
+def lint_mp4(*files: str, fix: bool = False) -> None:
+    """Detect if given MP4 files do not start with a moov atom."""
+    for file in files:
+        _lint_mp4(pathlib.Path(file), fix=fix)
+
+
+def _lint_mp4(path: pathlib.Path, *, fix: bool) -> None:
+    path_escaped = escape(str(path))
+
+    if path.suffix != ".mp4":
+        console.print(
+            f"[red]ERROR[/red]Not an MP4: [cyan]{path_escaped}[/cyan]",
+        )
+        return
+
+    with path.open("rb") as f:
+        first_50_bytes = f.read(50)
+        is_faststart = b"moov" in first_50_bytes
+
+    console.print(f"[cyan]{path_escaped}[/cyan]: ", end="")
+    if not is_faststart:
+        console.print("[red]moov not near start[/red]")
+        if fix:
+            console.print("[yellow]Fixing...[/yellow]")
+            _fix(path)
+    else:
+        console.print("[green]all good![/green]")
+
+
+def _fix(path: pathlib.Path) -> None:
+    output_name = f"{path.stem}_fast.mp4"
+    output_path = path.parent / output_name
+
+    ffmpeg_args = ["-i", str(path)]
+    ffmpeg_args.extend(["-c", "copy"])
+    ffmpeg_args.extend(["-movflags", "faststart"])
+    ffmpeg_args.append(str(output_path))
+    subprocess.check_call(["ffmpeg", *ffmpeg_args])
+
+    path.unlink()
+    output_path.rename(path)
+
+
+if __name__ == "__main__":
+    fire.Fire(lint_mp4)
